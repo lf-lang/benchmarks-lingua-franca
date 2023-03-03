d2994c95a5a41591d655489707d2e4ddb1f53ce5
Peter Donovan <peterdonovan@berkeley.edu>
2023-03-03 11:00:47 -0800

    Get some concurrency benchmarks to compile.
    This is the first 5 benchmarks (alphabetically).

--- C/Savina/src/include/PseudoRandom.h
+++ C/Savina/src/include/PseudoRandom.h
@@ -15,14 +15,14 @@ typedef struct PseudoRandom {
 /**
  * Initialize the random number generator to the specified seed.
  */
-void initPseudoRandom(struct PseudoRandom* random, long seed) {
+static void initPseudoRandom(struct PseudoRandom* random, long seed) {
     random->mValue = seed;
 }
 
 /**
  * Return a random number between 0 and 65535
  */
-long nextLong(struct PseudoRandom *random) {
+static long nextLong(struct PseudoRandom *random) {
     random->mValue = ((random->mValue * 1309) + 13849) & 65535;
     return random->mValue;
 }
@@ -30,7 +30,7 @@ long nextLong(struct PseudoRandom *random) {
 /**
  * Return a random number between 0 and 65535
  */
-int nextInt(struct PseudoRandom *random) {
+static int nextInt(struct PseudoRandom *random) {
     return (int)nextLong(random);
 }
 
@@ -38,14 +38,14 @@ int nextInt(struct PseudoRandom *random) {
  * Return a random number between 1.0/65536
  * and 1.0.
  */
-double nextDouble(struct PseudoRandom *random) {
+static double nextDouble(struct PseudoRandom *random) {
     return 1.0 / (nextLong(random) + 1);
 }
 
 /**
  * Return a random number between 0 and exclusive_max - 1.
  */
-int nextIntEMax(struct PseudoRandom *random, int exclusive_max) {
+static int nextIntEMax(struct PseudoRandom *random, int exclusive_max) {
     return nextInt(random) % exclusive_max;
 }
 
